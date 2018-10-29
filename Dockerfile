e4c367e0f16c808976f9d06f88ec335d0d042ad4
Chris Chen <chrischen79@gmail.com>
2018-10-29 16:32:08 +1100

    build dockerized express app

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,21 @@
 # node version
-FROM node:10-alpine
+FROM node:10.12.0-alpine
 
-# create app directory in container
-RUN mkdir -p /app
-
-# set /app directory as default working directory
+# create and set /app directory as default working directory
 WORKDIR /app
+
+# Install app dependencies
+# A wildcard is used to ensure both package.json AND package-lock.json are copied
+COPY package*.json ./
+
+# install npm packages
+RUN npm install --only=production && npm cache clean --force
 
 # copy source files
 COPY . .
 
-# install npm packages
-RUN npm install
-
-# expose port 3000
-EXPOSE 3000
+# expose port 4040
+EXPOSE 4040
 
 # cmd to start service
-CMD [ "node", "index.js" ]
+CMD [ "node", "src/index.js" ]
---
+++ src/index.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const util = require('util');
+
+// config should be imported before importing any other file
+const config = require('./config/config');
+const app = require('./config/express');
+
+// debug output with nice prefix
+const databaseDebug = require('debug')('otg:database');
+
+// make bluebird default Promise
+Promise = require('bluebird'); // eslint-disable-line no-global-assign
+
+// plugin bluebird promise in mongoose
+mongoose.Promise = Promise;
+
+// connect to mongo db
+const mongoUri = config.mongoURI;
+mongoose.connect(
+  mongoUri,
+  { useNewUrlParser: true },
+);
+
+mongoose.connection.on('error', () => {
+  throw new Error(`unable to connect to database: ${mongoUri}`);
+});
+
+// print mongoose logs in dev env
+if (config.mongooseDebug) {
+  mongoose.set('debug', (collectionName, method, query, doc) => {
+    databaseDebug(`${collectionName}.${method}`, util.inspect(query, false, 20), doc);
+  });
+}
+
+// module.parent check is required to support mocha watch
+// src: https://github.com/mochajs/mocha/issues/1912
+if (!module.parent) {
+  // listen on port config.port
+  app.listen(config.port, () => {
+    console.info(`server started on port ${config.port} (${config.env})`); // eslint-disable-line no-console
+  });
+}
+
+module.exports = app;
