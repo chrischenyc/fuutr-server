213fe7abb19dd5eceffa9704e9ab0c420f4177bd
Chris Chen <chrischen79@gmail.com>
2018-11-08 09:53:57 +1100

    update authenticate middleware

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -6,39 +6,46 @@ const APIError = require('../helpers/api-error');
 const User = require('../models/user');
 
 // middleware that validate JWT token in request headers "Authorization"
+
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
+
+  const bearer = authorization && authorization.split(' ')[0];
   const token = authorization && authorization.split(' ')[1];
 
-  if (token) {
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-      if (err) {
-        next(new APIError('wrong token', httpStatus.UNAUTHORIZED));
-      } else {
-        const { _id } = decoded;
-        User.findOne({ _id })
-          .select({
-            email: 1,
-            phoneNumber: 1,
-            countryCode: 1,
-            displayName: 1,
-            photo: 1,
-          })
-          .exec()
-          .then((user) => {
-            if (user) {
-              req.user = user;
-              next();
-            } else {
-              next(new APIError(`can't find user with _id ${_id}`, httpStatus.UNAUTHORIZED));
-            }
-          })
-          .catch(() => {
-            next(new APIError("can't query user", httpStatus.INTERNAL_SERVER_ERROR));
-          });
-      }
-    });
-  } else {
-    next(new APIError('wrong token', httpStatus.UNAUTHORIZED));
+  if (bearer !== 'Bearer' || !token) {
+    res.status(httpStatus.UNAUTHORIZED).send();
+    return;
   }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    const { _id } = decoded;
+
+    if (err || !_id) {
+      res.status(httpStatus.UNAUTHORIZED).send();
+      return;
+    }
+
+    User.findOne({ _id })
+      .select({
+        email: 1,
+        phoneNumber: 1,
+        countryCode: 1,
+        displayName: 1,
+        photo: 1,
+      })
+      .exec()
+      .then((user) => {
+        if (user) {
+          // inject found User record in request as .user
+          req.user = user;
+          next();
+        } else {
+          res.status(httpStatus.UNAUTHORIZED).send();
+        }
+      })
+      .catch(() => {
+        res.status(httpStatus.UNAUTHORIZED).send();
+      });
+  });
 };
