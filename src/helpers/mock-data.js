697f1f34381bbd84d5c7b8c27a100d2076b39fab
Chris Chen <chrischen79@gmail.com>
2018-11-13 14:44:13 +1100

    mocking scooter search result

--- src/helpers/mock-data.js
+++ src/helpers/mock-data.js
@@ -7,24 +7,30 @@ exports.mockScooters = (latitude, longitude, radius) => {
   const longitudeFloat = parseFloat(longitude);
   const radiusFloat = parseFloat(radius);
 
-  const numberOfScooters = Math.floor(radiusFloat * randomFromInterval(1, 9));
+  // filter invalid coordinates
+  if (latitudeFloat < -90 || latitudeFloat > 90 || longitudeFloat < -180 || longitudeFloat > 180) {
+    return [];
+  }
+
   const scooters = [];
+  const numberOfScooters = randomFromInterval(100, 500);
+  const latitudeOffset = radiusFloat / 110.574;
+  const longitudeOffset = radiusFloat / (111.32 * Math.cos(latitudeFloat));
 
   while (scooters.length < numberOfScooters) {
     scooters.push({
       iotCode: uuid.v4().split('-')[0],
       vehicleCode: uuid.v4().split('-')[0],
       powerPercent: Math.floor(randomFromInterval(10, 100)),
-      latitude: randomFromInterval(
-        latitudeFloat - radiusFloat / 110.574,
-        latitudeFloat + radiusFloat / 110.574
-      ),
+      latitude: randomFromInterval(latitudeFloat - latitudeOffset, latitudeFloat + latitudeOffset),
       longitude: randomFromInterval(
-        longitudeFloat - 111.32 * Math.cos(latitudeFloat),
-        longitudeFloat + 111.32 * Math.cos(latitudeFloat)
+        longitudeFloat - longitudeOffset,
+        longitudeFloat + longitudeOffset
       ),
     });
   }
+
+  console.log(scooters);
 
   return scooters;
 };
