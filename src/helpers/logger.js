77125a21a81134cffbd1b8c39ea02cb55d3ec65f
Chris Chen <chrischen79@gmail.com>
2018-11-25 10:46:36 +1100

    log to cloud watch

--- src/helpers/logger.js
+++ src/helpers/logger.js
@@ -1,4 +1,5 @@
 const winston = require('winston');
+const CloudWatchTransport = require('winston-aws-cloudwatch');
 
 const logger = winston.createLogger({
   level: 'info',
@@ -9,13 +10,32 @@ const logger = winston.createLogger({
   ],
 });
 
-//
 // If we're not in production then log to the `console` too
-//
 if (process.env.NODE_ENV !== 'production') {
   logger.add(
     new winston.transports.Console({
       format: winston.format.combine(winston.format.colorize(), winston.format.simple()),
+    })
+  );
+}
+
+// log to CloudWatch when in staging or production
+if (process.env.NODE_ENV === 'staging' || process.env.NODE_ENV === 'production') {
+  logger.add(
+    new CloudWatchTransport({
+      logGroupName: process.env.AWS_CLOUD_WATCH_GROUP,
+      logStreamName: process.env.AWS_CLOUD_WATCH_STREAM,
+      createLogGroup: true,
+      createLogStream: true,
+      submissionInterval: 2000,
+      submissionRetryCount: 1,
+      batchSize: 20,
+      awsConfig: {
+        accessKeyId: process.env.AWS_CLOUD_WATCH_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_CLOUD_WATCH_ACCESS_SECRET,
+        region: process.env.AWS_REGION,
+      },
+      formatLog: item => `${item.level}: ${item.message} ${JSON.stringify(item.meta)}`,
     })
   );
 }
