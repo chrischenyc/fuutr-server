8d390afb535d5fb8070b69d09d709b4ac83100bd
Chris Chen <chrischen79@gmail.com>
2019-01-23 18:11:29 +1100

    define Zone model

---
+++ src/models/zone.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+require('mongoose-geojson-schema');
+
+const zoneSchema = new mongoose.Schema(
+  {
+    polygon: { type: mongoose.Schema.Types.Polygon },
+    active: { type: Boolean, required: true, default: false },
+    parking: { type: Boolean, required: true, default: false },
+    speedMode: { type: Number }, // speed mode (1: low speed; 2: medium speed; 3: high speed;)
+  },
+  { timestamps: true, versionKey: false }
+).index({ polygon: '2dsphere' });
+
+module.exports = mongoose.model('Ride', zoneSchema);
