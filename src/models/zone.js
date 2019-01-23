2ed6a395ddec9376f326007131d4700fc798bea9
Chris Chen <chrischen79@gmail.com>
2019-01-23 18:11:51 +1100

    update zone model

--- src/models/zone.js
+++ src/models/zone.js
@@ -7,6 +7,7 @@ const zoneSchema = new mongoose.Schema(
     active: { type: Boolean, required: true, default: false },
     parking: { type: Boolean, required: true, default: false },
     speedMode: { type: Number }, // speed mode (1: low speed; 2: medium speed; 3: high speed;)
+    note: { type: String },
   },
   { timestamps: true, versionKey: false }
 ).index({ polygon: '2dsphere' });
