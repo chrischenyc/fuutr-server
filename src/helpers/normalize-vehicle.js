94d0cdf4e562d4cc3f17ff8bad2d117b0d73404c
Chris Chen <chrischen79@gmail.com>
2019-02-01 21:34:14 +1100

    attempt to fix vehicle zero remainderRange

--- src/helpers/normalize-vehicle.js
+++ src/helpers/normalize-vehicle.js
@@ -3,7 +3,7 @@ module.exports = (vehicle, user) => {
   let result = {
     _id: vehicle._id,
     powerPercent: vehicle.powerPercent,
-    remainderRange: vehicle.remainderRange * 10.0,
+    remainderRange: vehicle.remainderRange,
     vehicleCode: `xxxx-${vehicle.vehicleCode.slice(-4)}`,
     longitude: vehicle.location.coordinates[0],
     latitude: vehicle.location.coordinates[1],
