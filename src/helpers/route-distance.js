2a35effda0805e4861329db9c788a694c29e681d
Chris Chen <chrischen79@gmail.com>
2019-02-18 09:51:58 +1100

    fix distance unit

--- src/helpers/route-distance.js
+++ src/helpers/route-distance.js
@@ -1,3 +1,12 @@
+// distance between two coordinates, unit meters
+function distanceBetween(lat1, lon1, lat2, lon2) {
+  const p = 0.017453292519943295; // Math.PI / 180
+  const c = Math.cos;
+  const a = 0.5 - c((lat2 - lat1) * p) / 2 + (c(lat1 * p) * c(lat2 * p) * (1 - c((lon2 - lon1) * p))) / 2;
+
+  return 12742000 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
+}
+
 module.exports = (coordinates) => {
   let distance = 0;
   for (let index = 0; index < coordinates.length - 1; index += 1) {
@@ -9,11 +18,3 @@ module.exports = (coordinates) => {
 
   return distance;
 };
-
-function distanceBetween(lat1, lon1, lat2, lon2) {
-  const p = 0.017453292519943295; // Math.PI / 180
-  const c = Math.cos;
-  const a = 0.5 - c((lat2 - lat1) * p) / 2 + (c(lat1 * p) * c(lat2 * p) * (1 - c((lon2 - lon1) * p))) / 2;
-
-  return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
-}
