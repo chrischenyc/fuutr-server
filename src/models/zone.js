4a3957ab320325f4ea8cc9d30e3d6ade63a46742
Chris Chen <chrischen79@gmail.com>
2019-02-22 11:01:28 +1100

    add riding field to Zone model

--- src/models/zone.js
+++ src/models/zone.js
@@ -5,8 +5,9 @@ const zoneSchema = new mongoose.Schema(
   {
     polygon: { type: mongoose.Schema.Types.Polygon, required: true },
     active: { type: Boolean, required: true, default: false },
+    riding: { type: Boolean, required: true, default: true }, // no-riding zone = no-parking and speed 1
     parking: { type: Boolean, required: true, default: false },
-    speedMode: { type: Number, required: true, default: 0 }, // speed mode (0: no restrict; 1: low speed; 2: medium speed; 3: high speed;)
+    speedMode: { type: Number, required: true, default: 0 }, // see Vehicle model
     note: { type: String },
   },
   { timestamps: true, versionKey: false }
